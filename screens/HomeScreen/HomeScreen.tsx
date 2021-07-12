a7ccffae7aa71068c108ff30839c489c2fcd6a79
Chris Clark <chris.clark@globalstripe.com>
2021-07-12 20:24:01 +0100

    Flatlist for Rows

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -8,14 +8,18 @@ import categories from '../../assets/data/categories';
 
 import HomeCategory from '../../components/HomeCategory'
 
-const firstCategory = categories.items[0];
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-   
-      <HomeCategory category={firstCategory}/>
-    
+
+      <FlatList
+        data={categories.items}
+        renderItem={({item}) => 
+           <HomeCategory category={item}/>
+        }
+      />
+
     </View>
   );
 }
